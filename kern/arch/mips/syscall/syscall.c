23e81834551ba31e5342e5f7adad41ba655f5a52
g95wang <g95wang@gmail.com>
2021-03-07 22:02:05 -0500

    execv

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -135,6 +135,9 @@ void syscall(struct trapframe *tf)
 	case SYS_fork:
 		err = sys_fork(tf, (pid_t *)&retval);
 		break;
+	case SYS_execv:
+		err = sys_execv((char *)tf->tf_a0, (char **)tf->tf_a1);
+		break;
 
 #endif
 
--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -11,7 +11,9 @@
 #include <copyinout.h>
 #include <mips/trapframe.h>
 #include <array.h>
-#include <spl.h>
+#include <vm.h>
+#include <vfs.h>
+#include <kern/fcntl.h>
 #include "opt-A2.h"
 
 #if OPT_A2
@@ -67,6 +69,76 @@ int sys_fork(struct trapframe *tf, pid_t *retval)
   return 0;
 }
 
+int sys_execv(const char *program, char **args){
+  (void)args;
+
+	struct addrspace *as;
+	struct vnode *v;
+	vaddr_t entrypoint, stackptr;
+	int result;
+
+  int err;
+  size_t dummy;
+
+  char *progname = kmalloc(128 * sizeof(char));
+  if(progname == NULL){
+    return ENOMEM;
+  }
+  err = copyinstr((const_userptr_t)program, progname, 128, &dummy);
+  if(err){
+    kfree(progname);
+    return err;
+  }
+
+  // Copied from runprogram 
+	/* Open the file. */
+	result = vfs_open(progname, O_RDONLY, 0, &v);
+	if (result) {
+		return result;
+	}
+
+	/* Create a new address space. */
+	as = as_create();
+	if (as == NULL) {
+		vfs_close(v);
+		return ENOMEM;
+	}
+
+	/* Switch to it and activate it. */
+	curproc_setas(as);
+	as_activate();
+
+	/* Load the executable. */
+	result = load_elf(v, &entrypoint);
+	if (result) {
+		/* p_addrspace will go away when curproc is destroyed */
+		vfs_close(v);
+		return result;
+	}
+
+	/* Done with the file now. */
+	vfs_close(v);
+
+
+	/* Define the user stack in the address space */
+	result = as_define_stack(as, &stackptr);
+	if (result) {
+		/* p_addrspace will go away when curproc is destroyed */
+		return result;
+	}
+  // End of Copied from runprogram 
+
+
+
+	/* Warp to user mode. */
+	enter_new_process(0 /*argc*/, NULL /*userspace addr of argv*/,
+			  stackptr, entrypoint);
+	
+	/* enter_new_process does not return. */
+	panic("enter_new_process returned\n");
+	return EINVAL;
+}
+
 #endif
 
 /* this implementation of sys__exit does not do anything with the exit code */
@@ -98,17 +170,6 @@ void sys__exit(int exitcode)
   /* note: curproc cannot be used after this call */
   proc_remthread(curthread);
 #if OPT_A2
-  // // destroy Zombie children
-  // for (unsigned i = 0; i < array_num(p->children); i++)
-  // {
-  //   struct proc *child = (struct proc *)array_get(p->children, i);
-  //   if (child != NULL && child->status != Alive)
-  //   {
-  //     proc_destroy(child);
-  //     array_set(p->children, i, NULL);
-  //   }
-  // }
-
   if (p->parent == NULL || p->parent->status == Zombie)
   {
     proc_destroy(p);
